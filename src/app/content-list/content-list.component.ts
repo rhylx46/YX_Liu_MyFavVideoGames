56e303ca9c8ba06234fc5ce9b05cd1deb9104eb7
RYX46 <90352571+RYX46@users.noreply.github.com>
2022-03-02 21:29:36 -0500

    Modification for features

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -7,10 +7,9 @@ import { Content } from '../helper-files/content-interface';
   styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent implements OnInit {
- // myFavGames: Content[];
 @Input() item?: Content;
  msg?:string;
- color?: string;  
+ 
 
  myFavGames: Content[];
  constructor (){
@@ -97,27 +96,18 @@ gameFilter(game:string):any {
       
       if (game == this.myFavGames[i].title) {
         this.msg = 'Yes, there is the game you want';
-        this.color = "green";
-        //return [this.msg, this.color];
-
         return this.msg;
       } 
       else  {
         this.msg = 'No, can\'t find it';
-        this.color = "red";
+
       }
-      //console.log("See:", this.myFavGames[i].title);
+    
       
     }
 
 
-    // if (game == "Sekiro" || game == "Fallout 4" || game == "Resident Evil 2 Remake"|| game == "Kunoichi"|| game == "Fatal Frame: Mask of the Lunar Eclipse"|| game == "Devil Survivor Overclocked"|| game == "Devil Survivor 2 Record Breaker") {
-    //     this.msg = 'Yes, there is the game you want';
-    //     this.color = "green";
-    // } else {
-    //     this.msg = 'No, can\'t find it';
-    //     this.color = "red";
-    // }
+
 
     
 }
--- src/app/create-content/create-content.component.ts
+++ src/app/create-content/create-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewChild, ElementRef} from '@angular/core';
 import { Content } from '../helper-files/content-interface';
 
 
@@ -8,39 +8,28 @@ import { Content } from '../helper-files/content-interface';
   styleUrls: ['./create-content.component.scss']
 })
 export class CreateContentComponent implements OnInit {
+  @ViewChild('searchInput') searchInput?: ElementRef;
+  @ViewChild('searchInput1') searchInput1?: ElementRef;
+  @ViewChild('searchInput2') searchInput2?: ElementRef;
+  @ViewChild('searchInput3') searchInput3?: ElementRef;
+  @ViewChild('searchInput4') searchInput4?: ElementRef;
+  @ViewChild('searchInput5') searchInput5?: ElementRef;
+  @ViewChild('searchInput6') searchInput6?: ElementRef;
   @Output() newGameEvent: EventEmitter<Content> = new EventEmitter<Content>();
+ 
   newGame?: Content;
-  newPromise?: any;
+  ss?:string;
   result?: boolean;
-  constructor() {
 
 
-
-   }
-
-  hasLog() {
-
-    this.newPromise = new Promise((success, fail) => {
-
-    if (this.result == true) {
-        success('The addition is successful!');
-    } else {
-        
-        fail('The addition failed!');
-    }
-  });
-
-    this.newPromise.then(function(successEvent:any) {
-        console.log(successEvent);
-    }).catch(function(failureEvent:any) {
-            
-        const element: HTMLElement = document.getElementById('alert') as HTMLElement
-        element.innerHTML = failureEvent;
-    });
+  constructor(element: ElementRef) {
+    console.log(element.nativeElement);
   }
-
+  
   ngOnInit(): void {
   }
+
+ 
 
   addGame(id: string, title: string, description: string, creator: string, imgURL: string, type: string, tags: string): any {
     this.newGame = {
@@ -53,7 +42,7 @@ export class CreateContentComponent implements OnInit {
       tags: tags.split(",")
     };
 
-    
+    const self = this;
 
     if (this.newGame != null && this.newGame.id.toString() != '' && this.newGame.title != '' && this.newGame.description != '' && this.newGame.creator != '') {
 
@@ -67,8 +56,55 @@ export class CreateContentComponent implements OnInit {
     }
 
       
-    this.hasLog();
+    // this.hasLog();
 
+    
+    let newPromise:any;
+
+    
+    newPromise = new Promise((success, fail) => {
+
+    if (this.result == true) {
+        success('The addition is successful!');
+    } else {
+        
+        fail('The addition failed.');
+    }
+  });
+
+    
+
+   newPromise.then(function(successEvent:string) {
+        console.log(successEvent);
+        self.ss = '';
+        self.searchInput!.nativeElement!.value = '';
+        self.searchInput1!.nativeElement!.value = '';
+        self.searchInput2!.nativeElement!.value = '';
+        self.searchInput3!.nativeElement!.value = '';
+        self.searchInput4!.nativeElement!.value = '';
+        self.searchInput5!.nativeElement!.value = '';
+        self.searchInput6!.nativeElement!.value = '';
+        // console.log(self.searchInput?.nativeElement.value);
+    }).catch(
+      
+      function(failureEvent:any) {
+            
+        // const element: HTMLElement = document.getElementById('alert') as HTMLElement
+        // element.innerHTML = failureEvent;
+
+        self.ss = failureEvent;
+        
+    });
+
+
+    
   }
+
+  // clear():any {
+    
+  //   this.searchInput?.nativeElement?.innerHTML?? '';
+  // }
+
+  
 
 }
