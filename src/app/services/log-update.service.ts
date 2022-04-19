be3ff27c99794b70faaf344b73bfe1254762a9b5
RYX46 <90352571+RYX46@users.noreply.github.com>
2022-04-18 21:57:32 -0400

    Updated for test

--- src/app/services/log-update.service.ts
+++ src/app/services/log-update.service.ts
@@ -1,28 +1,35 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { SwUpdate } from '@angular/service-worker';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogUpdateService {
 
-  constructor(private updates: SwUpdate) { }
+  constructor(private updates: SwUpdate, private messageService: MessageService, private snackbar: MatSnackBar) { }
 
   public init() {
     this.updates.versionUpdates.subscribe(event => {
-      console.log("check for changes");
+      console.log("A version update potentially happened", event);
       switch (event.type) {
         case 'VERSION_DETECTED':
-          console.log(`Downloading new app version:
-    ${event.version.hash}`);
+          this.messageService.add(`Downloading new app version: ${event.version.hash}`);
           break;
         case 'VERSION_READY':
-          console.log(`Current app version:
-    ${event.currentVersion.hash}`);
-          console.log(`New app version ready for use:
-    ${event.latestVersion.hash}`);
-          this.updates.activateUpdate().then(() =>
-            document.location.reload());
+          this.messageService.add(`Current app version: ${event.currentVersion.hash}`);
+          this.messageService.add(`New app version ready for use: ${event.latestVersion.hash}`);
+          let snackBarRef = this.snackbar.open("An update is ready for you!", "Update App");
+          snackBarRef.onAction().subscribe(() => {
+            this.updates.activateUpdate().then(() => {
+              
+              document.location.reload();
+            });
+          });
+
+
+
           break;
       }
     });
